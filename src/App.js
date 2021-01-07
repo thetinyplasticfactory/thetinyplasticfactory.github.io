7a71fa5fec4487dbccac38f0240f1f38bf8b0b64
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2021-01-07 21:29:55 +1300

    Above the fold mostly done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React from "react";
-import logo from "./logo.svg";
+import styled from "styled-components";
 import "./App.scss";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import coverImg from "./imgs/tpf-cover.png";
+import coverImg from "./imgs/hero.png";
 import AboutSection from "./sections/About/About";
 import CollectionsSection from "./sections/Collections/Collections";
 import SalesSection from "./sections/Sales/Sales";
 import ContactSection from "./sections/Contact/Contact";
 import PartnersSection from "./sections/Partners/Partners";
+import SocialsBlock from "./components/SocialsBlock/SocialsBlock";
+
+const AboveTheFold = styled.section`
+  display: flex;
+  max-width: 1400px;
+  box-sizing: border-box;
+  padding: 0 6rem;
+  margin: 3rem auto 3rem;
+  align-items: center;
+  gap: 3rem;
+`;
+
+const HeroText = styled.h1`
+  font-size: 4rem;
+  line-height: 1.1;
+  margin: 0;
+  font-weight: 900;
+  letter-spacing: -4px;
+  color: #3e9a6e;
+`;
+
+const HeroBlurb = styled.h2`
+  margin: 0.5rem 0 0;
+`;
+const Left = styled.div`
+  flex: 1 0 calc(40% - 3rem);
+`;
+
+const HeroImage = styled.img`
+  display: block;
+`;
 
 const App = () => {
   return (
     <div className="App" id="top">
       <NavigationBar />
 
-      <img src={coverImg} className="cover-img" alt="cover-img" />
-      <AboutSection />
-      <CollectionsSection />
-      <SalesSection />
-      <ContactSection />
-      <PartnersSection />
+      <main>
+        <AboveTheFold>
+          <Left>
+            <HeroText>The Tiny Plastic Factory</HeroText>
+            <HeroBlurb>Plastic Recycling, Reinvented.</HeroBlurb>
+            <SocialsBlock />
+          </Left>
+          <HeroImage src={coverImg} alt="Hero image" role="presentation" />
+        </AboveTheFold>
+        <AboutSection />
+        <CollectionsSection />
+        <SalesSection />
+        <ContactSection />
+        <PartnersSection />
+      </main>
     </div>
   );
 };
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,42 +1,91 @@
 import React from "react";
-import "./styles.scss";
 import logo from "../../imgs/tpf.png";
 import SocialsBlock from "../SocialsBlock/SocialsBlock";
+import styled from "styled-components";
+
+const Logo = styled.img`
+  width: 8rem;
+`;
+
+const LogoContainer = styled.div`
+  width: 45%;
+`;
+
+const Header = styled.header`
+  display: flex;
+  margin: 0;
+  max-width: 1400px;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 6rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Nav = styled.nav`
+  flex: 1;
+`;
+
+const NavList = styled.ul`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0;
+`;
+
+const CallToActionLink = styled.a`
+  background: orange;
+  padding: 0.75rem 2rem;
+  margin: -1rem 1rem -2rem;
+  border-radius: 1rem;
+`;
 
 const NavigationBar = () => {
   return (
-    <header className="nav">
-      <div>
+    <Header>
+      <LogoContainer>
         <a href="#top" tabIndex={0}>
-          <img
-            src={logo}
-            alt="tiny-plastic-factory-logo"
-            className="nav-logo"
-          />
+          <Logo src={logo} alt="Tiny Plastic Factory Logo" />
         </a>
-      </div>
-      <div className="hide-on-mobile">
-        <a className="nav-link" href="#about" tabIndex={0}>
-          About
-        </a>
-        <a className="nav-link" href="#collections" tabIndex={0}>
-          Collection
-        </a>
-        <a className="nav-link" href="#sales" tabIndex={0}>
-          Sales
-        </a>
-        <a className="nav-link" href="#contact" tabIndex={0}>
-          Contact
-        </a>
-        <a className="nav-link" href="#partners" tabIndex={0}>
-          Partners
-        </a>
-      </div>
-      <div className="nav-socials">
-        <SocialsBlock />
-      </div>
-      
-    </header>
+      </LogoContainer>
+
+      <Nav>
+        <NavList className="hide-on-mobile">
+          <li>
+            <a href="#about" tabIndex={0}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#collections" tabIndex={0}>
+              Collection
+            </a>
+          </li>
+          <li>
+            <a href="#sales" tabIndex={0}>
+              Sales
+            </a>
+          </li>
+          <li>
+            <a href="#contact" tabIndex={0}>
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="#partners" tabIndex={0}>
+              Partners
+            </a>
+          </li>
+          <li>
+            <CallToActionLink href="#cta" tabIndex={0}>
+              Do stuff
+            </CallToActionLink>
+          </li>
+        </NavList>
+      </Nav>
+    </Header>
   );
 };
 
